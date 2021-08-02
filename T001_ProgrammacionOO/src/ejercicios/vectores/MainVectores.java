fe817ddc5dd37299e83bfd7f30c51fb2b89e86fb
usuario <usuario@192.168.1.2>
2021-08-02 18:57:03 -0500

    Prueba

---
+++ T001_ProgrammacionOO/src/ejercicios/vectores/MainVectores.java
@@ -0,0 +1,12 @@
+package ejercicios.vectores;
+
+public class MainVectores {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Declarar un vector mivector[10] de tipo int
+		int miVector[]= new int[10];
+
+	}
+
+}
