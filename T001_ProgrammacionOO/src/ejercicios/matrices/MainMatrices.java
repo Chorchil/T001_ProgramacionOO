d7b6223c5633dc3200c48c61cd101b6bb92edecd
usuario <usuario@192.168.1.2>
2021-08-02 20:04:06 -0500

    Taller de Arreglos

---
+++ T001_ProgrammacionOO/src/ejercicios/matrices/MainMatrices.java
@@ -0,0 +1,22 @@
+package ejercicios.matrices;
+
+public class MainMatrices {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 1. Declarar una matriz A de tipo char con 5 filas y 4 columnas
+		char A[][] = new char[5][4];
+		// 2. Declara una matriz B de tipo char con 5 filas y 6 columnas4
+		char B[][] = new char[5][6];
+		// 3. Declarar una matriz miMatriz de tipo Lon con 2 filas y 4 columnas
+		long miMatriz[][] = new long[2][4];
+		// 4. Declarar una matriz miMatriz1 de long fila 1 y 2 columnas
+		// y asignar a la poscicion [0][0]el valor de 2,poscicion [1][1]el valor de
+		// 8,poscicion [2][2]el valor de 2
+		long miMatriz1[][] = new long[1][2];
+		miMatriz1[0][0]=2;
+		miMatriz1[1][1]=8;
+		miMatriz1[2][2]=2;
+	}
+
+}
--- T001_ProgrammacionOO/src/ejercicios/vectores/MainVectores.java
+++ T001_ProgrammacionOO/src/ejercicios/vectores/MainVectores.java
@@ -4,8 +4,32 @@ public class MainVectores {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//Declarar un vector mivector[10] de tipo int
-		int miVector[]= new int[10];
+		// 1. Declarar un vector mivector[10] de tipo int
+		int miVector[] = new int[10];
+		// 2. Declarar un vector miVector1[5] de tipo char
+		char miVector1[] = new char[5];
+		// 3. Declarar un vector miVec[4] de tipo char, y asignamos la letra 'A' a la
+		// poscicion 0 y 2
+		char miVec[] = new char[4];
+		miVec[0] = 'A';
+		miVec[2] = 'A';
+		// 4. Declarar un vector miVec1[3] de tipo byte, y asignamos el numero0 a la
+		// posciciones 0,1,2y3
+		byte miVec1[] = new byte[3];
+		miVec1[0] = 0;
+		miVec1[1] = 0;
+		miVec1[2] = 0;
+		// miVec1[3]=0; esta es una mala asignacion por que esta fuera de rango
+		// 5. Declarar un vector miVec2[3] de tipo char, y asignamos la letra 'A' a la
+		// posciciones 0y1 y a la poscicion 2 le asignamos la letra 'B'
+		char miVec2[] = new char[3];
+		miVec2[0] = 'A';
+		miVec2[1] = 'A';
+		miVec2[2] = 'B';
+		miVec2[0] = 'Z';
+		System.out.println("Impresion 1: " + miVec2[0]);
+		System.out.println("Impresion 2: " + miVec2[2]);
+		
 
 	}
 
